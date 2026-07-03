0092b4c1cf8d511759bac9eb6e26fb78711ce179
Satyam-Gawali <satyamgawali100@gmail.com>
2026-07-03 22:07:47 +0530

    create class AppAssets

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -1,26 +1,28 @@
+import 'package:flutter/material.dart';
+
 abstract final class AppAssets {
-  // Adaptive Icon
   static const appLogo = 'assets/logos/app_logo.png';
   static const appLogoFg = 'assets/logos/app_logo_fg.png';
   static const appLogoBg = 'assets/logos/app_logo_bg.png';
 
-  // Badge
   static const badge = 'assets/logos/badge.png';
-
-  // Icon Only
   static const icon = 'assets/logos/logo.png';
 
-  // Horizontal Logos
   static const logoLight = 'assets/logos/yo_light.png';
   static const logoDark = 'assets/logos/yo_dark.png';
 
-  // Vertical Logos
   static const logoLightV = 'assets/logos/yo_light_v.png';
   static const logoDarkV = 'assets/logos/yo_dark_v.png';
 
-  static String logo(bool isDark) =>
-      isDark ? logoDark : logoLight;
+  static String logo(BuildContext context) {
+    return Theme.of(context).brightness == Brightness.dark
+        ? logoDark
+        : logoLight;
+  }
 
-  static String logoV(bool isDark) =>
-      isDark ? logoDarkV : logoLightV;
+  static String logoV(BuildContext context) {
+    return Theme.of(context).brightness == Brightness.dark
+        ? logoDarkV
+        : logoLightV;
+  }
 }
